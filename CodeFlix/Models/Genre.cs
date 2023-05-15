af67891af56684193bc6865a7703675f0877d3ae
Rafael Haziel <hazieldefato@gmail.com>
2023-05-15 08:39:51 -0300

    Inserção de Anotação de Dados nas classes do modelo.

--- CodeFlix/Models/Genre.cs
+++ CodeFlix/Models/Genre.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CodeFlix.Models;
 
 public class Genre
 {
-    public byte Id { get; set; }    
+    [Key] // É uma anotação para chave primária.
+    public byte Id { get; set; }  
+
+    [Required] // Requerido - Not Null no banco; Torna o nome obrigatório.
+    [StringLength(30)] // Tamanho máximo da propriedade.  
     public string Name { get; set; }    
 }
